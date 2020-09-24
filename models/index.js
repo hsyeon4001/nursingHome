b56dc078b9c9dced848042f64336621d11256207
hsyeon4001 <hsyeon4001@gmail.com>
2020-09-24 23:37:20 +0900

    CREATE: 정적 페이지 연결, 게시판 및 갤러리 구현중

--- models/index.js
+++ models/index.js
@@ -7,7 +7,20 @@ const sequelize = new Sequelize(
 	config.database,
 	config.username,
 	config.password,
-	config
+	{
+		host: '127.0.0.1',
+		dialect: 'mysql',
+		dialectOptions: {
+			charset: 'utf8mb4',
+			dateStrings: true,
+			typeCast: true
+		},
+		pool: {
+			max: 20,   // 최대 유지 connection 수
+			min: 5,    // 최소 유지 connection 수
+			idle: 60000 // connection을 몇ms까지 대기시킬 것인가 (이후엔 버려짐)
+		}
+	}
 );
 
 db.sequelize = sequelize;
--- routes/page.js
+++ routes/page.js
@@ -9,17 +9,18 @@ router.get("/", (req, res) => {
 	res.render("main");
 });
 
-router.get("/gallery", async (req, res) => {
-	let page = req.query.page;
+router.get("/gallery/:page", async (req, res) => {
+	let page = req.params.page;
 	let offset = 0;
 
 	try {
+
 		if (page > 1) {
 			offset = 6 * (page - 1);
 		}
 
 		const posts = await Post.findAndCountAll({
-			attributes: ["img", "title", "createdAt"],
+			attributes: ["postId", "img", "title", "created_at"],
 			offset: offset,
 			limit: 6,
 		});
@@ -27,16 +28,25 @@ router.get("/gallery", async (req, res) => {
 		const imgs = [];
 		const titles = [];
 		const dates = [];
-		for (let i = 0; i < 6; i++) {
+		const ids = [];
+
+		for (let i = 0; i < posts.rows.length; i++) {
+			let date = (posts.rows[i].created_at).substring(0, 10);
+			dates.push(date);
 			imgs.push(posts.rows[i].img);
 			titles.push(posts.rows[i].title);
-			dates.push(posts.rows[i].createdAt);
+			ids.push(posts.rows[i].postId);
 		}
-		console.log(posts.count);
+
+		console.log(ids);
 		res.render("gallery", {
-			title: titles,
+			sub: "Gallery",
+			path: "늘봄소식",
+			headline: "갤러리",
+			ids: ids,
+			titles: titles,
 			thumb: imgs,
-			date: dates,
+			dates: dates,
 			posts: posts.count,
 		});
 	} catch {
@@ -47,7 +57,7 @@ router.get("/gallery", async (req, res) => {
 
 router.get(
 	"/post",
-	passport.authenticate("jwt", { session: false }),
+	// passport.authenticate("jwt", { session: false }),
 	(req, res) => {
 		res.render("post");
 	}
@@ -55,6 +65,38 @@ router.get(
 
 router.get("/join", (req, res) => {
 	res.render("join");
+});
+
+router.get("/introduction", (req, res) => {
+	res.render("sub", { sub: "Introduction", path: "시설소개", headline: "늘봄소개" });
+});
+
+router.get("/way", (req, res) => {
+	res.render("sub", { sub: "Way", path: "시설소개", headline: "오시는길" });
+});
+
+router.get("/admit", (req, res) => {
+	res.render("sub", { sub: "Admit", path: "입소안내", headline: "입소절차" });
+});
+
+router.get("/space", (req, res) => {
+	res.render("sub", { sub: "Space", path: "입소안내", headline: "늘봄공간" });
+});
+
+router.get("/notice", (req, res) => {
+	res.render("sub", { sub: "Notice", path: "늘봄소식", headline: "공지사항" });
+});
+
+router.get("/board", (req, res) => {
+	res.render("sub", { sub: "Board", path: "늘봄소식", headline: "게시판" });
+});
+
+router.get("/agency", (req, res) => {
+	res.render("sub", { sub: "Agency", path: "문의하기", headline: "유관기관" });
+});
+
+router.get("/counseling", (req, res) => {
+	res.render("sub", { sub: "Counseling", path: "문의하기", headline: "상담신청" });
 });
 
 router.post("/auth/tokens", authTokenController.create);
